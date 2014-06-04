66e45304e9a4db73d18623665d35462d0d7c3495
AlienJim <burton.alexander102@gmail.com>
2014-06-04 14:37:55 -0400

    Just a Test
    Added a Game Manager script to the project just to see if it works
    correctly.

---
+++ IronPack/Assets/Scripts/Game Manager.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Game Manager for Iron Pack
+ * Author: Alexander Burton
+ * Date: June 4th 2014
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance {get {return Instance;} }
+	public static GameManager instance = null;
+
+
+	 void Awake()
+	{
+		if ( instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
