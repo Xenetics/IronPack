fd65d115e11efe37e5eb17b661f4264f7db6a6d1
AlienJim <burton.alexander102@gmail.com>
2014-06-27 15:57:35 -0400

    Revert "Workingish Radial Menu"
    This reverts commit 25844a5a12ba13bf144897e7e07ff8c784cc386d.

--- IronPack/Assets/Scripts/Radial.cs
+++ IronPack/Assets/Scripts/Radial.cs
@@ -5,12 +5,10 @@ using System.Collections.Generic;
 
 public class Radial : MonoBehaviour 
 {
-	List<Transform> wolves;
+	List<GameObject> wolves;
 	public bool radialShow = false;
-
-	private Vector2 wPos;
-	private Vector2 wolfPos;
-	private float mag;
+	Vector2 wPos;
+	float mag;
 	
 	public Texture backgroundTexture;
 	
@@ -26,65 +24,45 @@ public class Radial : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector2 mPos = GetMouseInWorld(-2f);
+		Vector2 mPos = Input.mousePosition;
 		//wolves[0].transform.position;
 
 		if(Input.GetMouseButtonDown(0))
 		{
 			UpdateWolves();
-			//find the wolf you clicked on
 		}
 
-		if (Input.GetMouseButton (0) && !radialShow) 
+		if (Input.GetMouseButton (0)) 
 		{
 			UpdateWolves();
 			for(int i = 0 ; i < wolves.Count ; ++i)
 			{
-				wolfPos = wolves[i].position;
-				if( (wolfPos - mPos).magnitude < 0.8f) 
+				wPos = wolves[i].transform.position;
+				if((mPos - wPos).magnitude < 0.85f)
 				{
+					print("GAH!");
 					radialShow = true;
-					menuButtonX = Input.mousePosition.x;
-					menuButtonY = Input.mousePosition.y;
 				}
 			}
 		}
-
-		if (Input.GetMouseButtonUp(0))
+		
+		if(Input.GetMouseButtonUp(0))
 		{
-			if (radialShow == true)
-			{
-				float dir = GetVectorDirection(mPos - wolfPos);
-				if(dir > 0 && dir < 90)
-				{
-					//command for pointing top right (move)
-				}
-				else if(dir > 90 && dir < 180)
-				{
-					//command for pointing top left(special)
-				}
-				else if(dir > 180 && dir < 270)
-				{
-					//command for pointing bottom left(stay)
-				}
-				else if(dir > 270 && dir < 360)
-				{
-					//command for pointing bottom right(attack)           
-				}
-			}
 			radialShow = false;
 		}
+
 	}
 
-	void OnGUI()
+	void OnGui()
 	{
-		//GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), backgroundTexture);
 		if(radialShow)
 		{
-			GUI.Button (new Rect (menuButtonX + 100, menuButtonY + 100, Screen.width * .1f, Screen.height * .1f), "ATK");
-			GUI.Button (new Rect (menuButtonX + 100, menuButtonY - 100, Screen.width * .1f, Screen.height * .1f), "Move");
-			GUI.Button (new Rect (menuButtonX - 100, menuButtonY + 100, Screen.width * .1f, Screen.height * .1f), "Stay");
-			GUI.Button (new Rect (menuButtonX - 100, menuButtonY - 100, Screen.width * .1f, Screen.height * .1f), "SPCL");
+
+			GUI.Button (new Rect (Screen.width * menuButtonX, Screen.height * menuButtonY, Screen.width * .1f, Screen.height * .5f), "ATK");
+			GUI.Button (new Rect (Screen.width * menuButtonX, Screen.height * menuButtonY, Screen.width * .5f, Screen.height * .1f), "Move");
+			GUI.Button (new Rect (Screen.width * menuButtonX, Screen.height * menuButtonY, Screen.width * .1f, Screen.height * .5f), "Stay");
+			GUI.Button (new Rect (Screen.width * menuButtonX, Screen.height * menuButtonY, Screen.width * .5f, Screen.height * .1f), "SPCL");
 		}
 	}
 
@@ -92,47 +70,11 @@ public class Radial : MonoBehaviour
 	{
 		if (UnitManager.Instance.getWolves () != null)
 		{
-			List<GameObject> temp = UnitManager.Instance.getWolves ();
-			for(int i = 0 ; i < temp.Count ; ++i)
-			{
-				wolves.Add(temp[i].transform);
-			}
+			wolves = UnitManager.Instance.getWolves ();
 		} 
 		else 
 		{
-			wolves = new List<Transform>();
+			wolves = new List<GameObject>();
 		}
 	}
-
-	private Vector2 GetMouseInWorld( float z)
-	{
-		Vector3 v3 = Input.mousePosition;
-		v3.z = z;
-		v3 = Camera.main.ScreenToWorldPoint(v3);
-
-		return v3;
-	}
-
-	private float GetVectorDirection(Vector2 v)
-	{
-		float ret = 0;
-		if(v.x > 0 && v.y > 0) //Q1
-		{
-			ret =  Mathf.Rad2Deg * (Mathf.Atan(v.y/v.x));
-		}
-		else if(v.x > 0 &&  v.y < 0) //Q2
-		{
-			ret = 360 + Mathf.Rad2Deg * (Mathf.Atan(v.y/v.x));
-		}
-		else if(v.x < 0 &&  v.y < 0) //Q3
-		{
-			ret = 180 + Mathf.Rad2Deg * (Mathf.Atan(v.y/v.x));
-		}
-		else if(v.x < 0 &&  v.y > 0)//Q4
-		{
-			ret = 90 - (-Mathf.Rad2Deg * (Mathf.Atan(v.y/v.x)) - 90);
-		}
-		return ret;
-	}
-
 }
